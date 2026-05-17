01e49b15da5345461d5682e806f475d328d6c3aa
ANATOL3 <anatole.coquelin@gmail.com>
2026-05-17 17:10:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,33 +13,4 @@ effacer_recherche.addEventListener("click", (event) => {
     event.stopPropagation();
     recherche.classList.remove("open");
     input_reset.value = "";
-})
-
-
-
-
-
-    /*
-    const recherche = document.querySelector(".recherche");
-
-    recherche.addEventListener("click", () => {
-        recherche.style.width = 300 + "px";
-        console.log("click réussi");
-    });
-
-    const cross = document.querySelector(".effacer-recherche");
-
-    cross.addEventListener("click", () => {
-        event.stopPropagation();
-        recherche.style.width = 30 + "px";
-    })
-    */
-
-
-    /*
-    recherche.addEventListener("click", () => {
-        recherche.classList.toggle("open");
-
-        console.log("open bar recherche réussi")
-    });
-    */
+});
